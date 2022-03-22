3bba3c908552aa75f2b579b39a87f5a296f2b62c
ept-huzaifa <noorhuzaifa11@gmail.com>
2022-03-22 18:45:17 +0500

    reformatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
 import { useState } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import FetchData from "./component/Data/FetchData";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className='App'>
-      <header className='App-header'>
-        <FetchData />
-      </header>
+      <FetchData />
     </div>
   );
 }
