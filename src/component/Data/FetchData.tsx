9a1fef7ddadd70ce4566573db338a2966a79714b
ept-huzaifa <noorhuzaifa11@gmail.com>
2022-03-22 18:31:22 +0500

    Reddit API data fetching

--- src/component/Data/FetchData.tsx
+++ src/component/Data/FetchData.tsx
@@ -1,9 +1,14 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import styles from "./FetchData.module.css";
 
 type post = {
   id: string;
   title: string;
+  url: string;
+  ups: number;
+  author: string;
+  selftext: string;
 };
 
 const FetchData = () => {
@@ -12,19 +17,27 @@ const FetchData = () => {
   useEffect(() => {
     axios.get(`https://www.reddit.com/r/reactjs.json`).then((res) => {
       const newPosts = res.data.data.children.map((obj: any) => obj.data);
-      console.log(res);
       setPosts(newPosts);
     });
   }, []);
 
   return (
     <>
-      <h1>r/Fecthing Data</h1>
-      <ul>
+      <h1>r/React Js</h1>
+      <div>
         {posts.map((post: post) => {
-          return <li key={post.id}>{post.title}</li>;
+          return (
+            <div className={styles.comp} key={post.id}>
+              <p className={styles.author}>Posted by u/{post.author}</p>
+              <a target='_blank' className={styles.a} href={post.url}>
+                {post.title}
+              </a>
+              <p className={styles.upvotes}>Total upvotes: {post.ups}</p>
+              <p className={styles.body}>{post.selftext}</p>
+            </div>
+          );
         })}
-      </ul>
+      </div>
     </>
   );
 };
